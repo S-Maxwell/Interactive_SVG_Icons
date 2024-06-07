99222e735d5a6429adc054a20137f76147cf5eab
S-Maxwell <156356305+S-Maxwell@users.noreply.github.com>
2024-06-07 16:48:38 -0400

    Update main.JS
    In this commit, the `main.js` file was created, and the coding requirements specified in the assignment were added.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 console.log('JavaScript is running!');
+
+const ingredients = document.querySelectorAll("#ingredients img");
+console.log(ingredients);
+
+function logId() {
+    console.log(this.id);
+}
+
+//short-hand
+ingredients.forEach(ingredients => ingredients.addEventListener("click", logId));
+
+//long-hand
+ingredients.forEach(function(ingredients){
+    ingredients.addEventListener("click", logId);
+})
